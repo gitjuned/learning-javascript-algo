b92cba1e86f6cd4265146dc27cbaf4a88e869db8
gitjuned <junaydk46@gmail.com>
2020-06-06 23:16:16 +0530

    largestOfFour: returns array containing largest number  from sub arrays

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-function findLongestWordLength(str) {
-  let array = str.split(" ");
+function largestOfFour(arr) {
+  let largest = 0;
+  let array;
+  arr.forEach((element) => {
+    element.forEach((item) => {
+      if (item > largest) largest = item;
+    });
+  });
 
-  let longest = 0;
-  array.forEach((element) => {
-    if (element.length > longest) {
-      longest = element.length;
+  arr.forEach((element) => {
+    if (element.includes(largest)) {
+      array = element;
     }
   });
-  return longest;
+  return array;
 }
 
-console.log(findLongestWordLength("one two three four"));
+console.log(
+  largestOfFour([
+    [4, 5, 100, 3],
+    [13, 27, 18, 26],
+    [32, 35, 3752, 39],
+    [1000, 2001, 857, 1],
+  ])
+);
